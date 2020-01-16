7b4ac292ce165d129baf973edc5c7cb03e733c26
Adam Stokes <battlemidget@users.noreply.github.com>
2020-01-16 12:08:49 -0500

    Update sync command
    Signed-off-by: Adam Stokes <battlemidget@users.noreply.github.com>

--- cmd/sync.go
+++ cmd/sync.go
@@ -23,14 +23,47 @@ THE SOFTWARE.
 package cmd
 
 import (
-
-	// "github.com/codeskyblue/go-sh"
-
+	"fmt"
+	"github.com/codeskyblue/go-sh"
 	"github.com/spf13/cobra"
 	jww "github.com/spf13/jwalterweatherman"
+	"gopkg.in/yaml.v2"
+	"io/ioutil"
+	"net/url"
+	"os"
+	"path/filepath"
+	"strings"
 )
 
 var spec []string
+
+type RepoSpec struct {
+	Repos []struct {
+		Name            string   `yaml:"name"`
+		Downstream      string   `yaml:"downstream"`
+		Upstream        string   `yaml:"upstream"`
+		Tags            []string `yaml:"tags,omitempty"`
+		NeedsStable     string   `yaml:"needs_stable,omitempty"`
+		NeedsTagging    string   `yaml:"needs_tagging,omitempty"`
+		ResourceBuildSh string   `yaml:"resource_build_sh,omitempty"`
+		Namespace       string   `yaml:"namespace,omitempty"`
+	} `yaml:"repos"`
+}
+
+func (c *RepoSpec) parse(specFile string) *RepoSpec {
+	filename, _ := filepath.Abs(specFile)
+	yamlFile, err := ioutil.ReadFile(filename)
+	if err != nil {
+		jww.ERROR.Printf("Failed to read %s %v", filename, err)
+		os.Exit(1)
+	}
+	err = yaml.Unmarshal(yamlFile, c)
+	if err != nil {
+		jww.ERROR.Printf("Failed to unmarshal: %v", err)
+		os.Exit(1)
+	}
+	return c
+}
 
 // syncCmd represents the sync command
 var syncCmd = &cobra.Command{
@@ -38,8 +71,49 @@ var syncCmd = &cobra.Command{
 	Short: "Sync upstream repos",
 	Long:  `Syncs upstream git repos with `,
 	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("Syncing upstream <-> downstream repositories")
+
+		var c RepoSpec
+		ghUser := url.QueryEscape(os.Getenv("CDKBOT_GH_USR"))
+		ghPass := url.QueryEscape(os.Getenv("CDKBOT_GH_PSW"))
 		for _, v := range spec {
-			jww.INFO.Printf("Cloning %s", v)
+			output := c.parse(v)
+			for _, r := range output.Repos {
+				tmpDir, err := ioutil.TempDir("", "reposync")
+				if err != nil {
+					jww.ERROR.Printf("Failed to create tempdir: %v", err)
+					os.Exit(1)
+				}
+				defer os.RemoveAll(tmpDir)
+				fmt.Println("- storing repos in ", tmpDir)
+
+				fmt.Printf("Processing: %s\n", r.Name)
+				uPath, err := url.Parse(r.Upstream)
+				if err != nil {
+					jww.ERROR.Printf("[Skipping] Problem reading url: %v", err)
+					continue
+				}
+				uPathStrip := strings.TrimRight(uPath.EscapedPath(), ".git")
+				uPathStrip = strings.TrimLeft(uPathStrip, "/")
+				if r.Downstream == uPathStrip {
+					fmt.Printf("[Skipping] %s == %s\n", r.Downstream, uPathStrip)
+					continue
+				}
+				cloneUrl := fmt.Sprintf("https://%s:%s@github.com/%s", ghUser, ghPass, r.Downstream)
+				sh.Command("git", "clone", cloneUrl, tmpDir).Run()
+
+				// Handle clone
+				session := sh.NewSession()
+				session.SetDir(tmpDir)
+				session.Command("git", "config", "user.email", "cdkbot@juju.solutions")
+				session.Command("git", "config", "user.name", "cdkbot")
+				session.Command("git", "config", "--global", "push.default", "simple")
+				session.Command("git", "remote", "add", "upstream", r.Upstream)
+				session.Command("git", "fetch", "upstream")
+				session.Command("git", "checkout", "master")
+				session.Command("git", "merge", "upstream/master")
+				session.Command("git", "push", "origin")
+			}
 		}
 	},
 }
--- go.mod
+++ go.mod
@@ -9,4 +9,5 @@ require (
 	github.com/spf13/jwalterweatherman v1.0.0
 	github.com/spf13/pflag v1.0.5 // indirect
 	github.com/spf13/viper v1.6.1
+	gopkg.in/yaml.v2 v2.2.4
 )
