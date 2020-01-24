7616224290fba385646dc4248303bb30b2c33765
Adam Stokes <battlemidget@users.noreply.github.com>
2020-01-24 10:50:13 -0500

    new mod
    Signed-off-by: Adam Stokes <battlemidget@users.noreply.github.com>

---
+++ snap/revision.go
@@ -0,0 +1,15 @@
+package snap
+
+import (
+	"errors"
+	"fmt"
+	"github.com/battlemidget/mf/common"
+
+	"github.com/codeskyblue/go-sh"
+	log "github.com/sirupsen/logrus"
+	"strings"
+)
+
+func Revisions(snap string, track string, arch string, excludePre bool) {
+	
+}
